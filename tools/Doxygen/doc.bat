47c0bbac32e3bb665c3c4897da7a10c0e4090e3f
Tedi <hkanelhs@yahoo.gr>
2017-05-12 15:30:47 +0300

    Fixxed error with Github pages

--- tools/Doxygen/doc.bat
+++ tools/Doxygen/doc.bat
@@ -4,8 +4,6 @@ echo This batch file runs the Doxygen program using the Doxyfile configuration
 echo file.
 echo ===================================================
 
-MKDIR "../../doc/"
-
 doxygen.exe "doxyfile" 1>NUL 2>"doxygen_stderr.txt"
 
 echo ===================================================
